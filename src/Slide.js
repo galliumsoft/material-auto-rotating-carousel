ef849e2c37d58bdcbec575dea48b23551722e5f9
Madhukar I B <galliumsoft@gmail.com>
2019-02-04 20:10:36 +0000

    bugs fixed

--- src/Slide.js
+++ src/Slide.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import blue from '@material-ui/core/colors/blue'
 import withStyles from '@material-ui/core/styles/withStyles'
